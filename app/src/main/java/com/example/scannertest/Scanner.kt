9efb6d352cd28ae0209129886a3e179cd0a31c31
Allan Davis <cajun.code@gmail.com>
2022-01-19 00:53:35 -0600

    created Scanner Activity

---
+++ app/src/main/java/com/example/scannertest/Scanner.kt
@@ -0,0 +1,11 @@
+package com.example.scannertest
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Scanner : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_scanner)
+    }
+}
